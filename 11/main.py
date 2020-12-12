d162fbfe161c7681ea932b0233867f10965b5f6c
Sam Choukri <schoukri@cordial.com>
2020-12-12 11:03:02 -0800

    cleanup

--- 11/main.py
+++ 11/main.py
@@ -3,30 +3,28 @@
 import sys, re
 
 class Grid:
+
     _toggle = {'L': '#', '#': 'L'}
     _offsets = [(-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1, 0)]
 
     def __init__(self, lines):
-        self.data = []
-        for y in range(len(lines)):
-            self.data.append(list(lines[y][x] for x in range(len(lines[y]))))
+        self.data = list([char for char in row] for row in lines)
         self.maxy = len(self.data) - 1
-        self.maxx = len(self.data[0]) - 1
+        self.maxx = max(len(row) for row in self.data) - 1
 
     def __repr__(self):
-        return "\n".join(list(''.join(row) for row in self.data))
+        return "\n".join(''.join(row) for row in self.data)
 
     def rearrange(self, adjacent_only, flip_occupied_min):
         flip = list()
-        for y in range(len(self.data)):
-            for x in range(len(self.data[y])):
-                status = self.data[y][x]
-                if status == '.':
+        for (y, row) in enumerate(self.data):
+            for (x, cell) in enumerate(row):
+                if cell == '.':
                     continue
                 count = self.visible_occupied(x, y, adjacent_only)
-                if status == 'L' and count == 0:
+                if cell == 'L' and count == 0:
                     flip.append((x, y))
-                elif status == '#' and count >= flip_occupied_min:
+                elif cell == '#' and count >= flip_occupied_min:
                     flip.append((x, y))
         for (x, y) in flip:
             self.data[y][x] = self._toggle[self.data[y][x]]
@@ -35,12 +33,8 @@ class Grid:
     def visible_occupied(self, x, y, adjacent_only=True):
         count = 0
         for (dx, dy) in self._offsets:
-            (ax, ay) = (x, y)
-            while True:
-                ax += dx
-                ay += dy
-                if ax < 0 or ax > self.maxx or ay < 0 or ay > self.maxy:
-                    break
+            (ax, ay) = (x + dx, y + dy)
+            while 0 <= ax <= self.maxx and 0 <= ay <= self.maxy:
                 if self.data[ay][ax] == '#':
                     count += 1
                     break
@@ -48,26 +42,22 @@ class Grid:
                     break
                 if adjacent_only:
                     break
+                ax += dx
+                ay += dy
         return count
 
     def total_occupied(self):
-        total = 0
-        for y in range(len(self.data)):
-            for x in range(len(self.data[y])):
-                if self.data[y][x] == '#':
-                    total += 1
-        return total
-
+        return sum(1 for row in self.data for seat in row if seat == '#')
 
 if __name__ == '__main__':
     lines = list(line for line in re.split("\n", sys.stdin.read().rstrip()))
 
     grid1 = Grid(lines)
-    while grid1.rearrange(True, 4) > 0:
+    while grid1.rearrange(adjacent_only=True, flip_occupied_min=4) > 0:
         pass
     print("PART 1:", grid1.total_occupied())
 
     grid2 = Grid(lines)
-    while grid2.rearrange(False, 5) > 0:
+    while grid2.rearrange(adjacent_only=False, flip_occupied_min=5) > 0:
         pass
     print("PART 2:", grid2.total_occupied())
--- 12/main.py
+++ 12/main.py
@@ -4,38 +4,30 @@ import sys, re
 from collections import namedtuple
 
 class Position:
-    def __init__(self, x, y, heading=90):
+    def __init__(self, x, y, direction='E'):
         self.x = x
         self.y = y
-        self.heading = heading
+        self.heading = self.to_degrees(direction)
     def turn(self, direction, value):
         if direction == 'R':
             self.heading = (self.heading + value) % 360
         elif direction == 'L':
             self.heading = (self.heading - value) % 360
-    def move(self, direction, value):
-        if direction == 'N':
+    def move(self, heading, value):
+        if heading == 0:
             self.y -= value
-        elif direction == 'E':
+        elif heading == 90:
             self.x += value
-        elif direction == 'S':
+        elif heading == 180:
             self.y += value
-        elif direction == 'W':
+        elif heading == 270:
             self.x -= value
     def forward(self, value):
-        if self.heading == 0:
-            self.y -= value
-        elif self.heading == 90:
-            self.x += value
-        elif self.heading == 180:
-            self.y += value
-        elif self.heading == 270:
-            self.x -= value
+        self.move(self.heading, value)
     def rotate_around(self, origin, direction, value):
         # convert 'L' direction to equivalent value for 'R'
         if direction == 'L':
             value = 360 - value
-
         # rotate right 90 degrees at a time
         while value in (90, 180, 270):
             dx = self.x - origin.x
@@ -45,15 +37,20 @@ class Position:
             self.y = origin.y + dy
             value -= 90
 
+    @classmethod
+    def to_degrees(self, direction):
+        degrees = {'N': 0, 'E': 90, 'S': 180, 'W': 270}
+        return degrees[direction]
+
 
 def part_2(steps):
-    ship = Position(x=0, y=0, heading=90)
+    ship = Position(x=0, y=0, direction='E')
     waypt = Position(x=10, y=-1)
     for step in steps:
-        if step.action in ('R', 'L'):
+        if step.action in 'RL':
             waypt.rotate_around(ship, step.action, step.value)
-        elif step.action in ('N', 'E', 'S', 'W'):
-            waypt.move(step.action, step.value)
+        elif step.action in 'NSEW':
+            waypt.move(waypt.to_degrees(step.action), step.value)
         elif step.action == 'F':
             dx = waypt.x - ship.x
             dy = waypt.y - ship.y
@@ -64,31 +61,19 @@ def part_2(steps):
     return abs(ship.x) + abs(ship.y)
 
 def part_1(steps):
-    #degrees = {'N': 0, 'E': 90, 'S': 180, 'W': 270}
-    ship = Position(x=0, y=0, heading=90)
+    ship = Position(x=0, y=0, direction='E')
     for step in steps:
-        if step.action in ('R', 'L'):
+        if step.action in 'RL':
             ship.turn(step.action, step.value)
-        elif step.action == 'N':
-            ship.y -= step.value
-        elif step.action == 'E':
-            ship.x += step.value
-        elif step.action == 'S':
-            ship.y += step.value
-        elif step.action == 'W':
-            ship.x -= step.value
+        elif step.action in 'NSEW':
+            ship.move(ship.to_degrees(step.action), step.value)
         elif step.action == 'F':
             ship.forward(step.value)
     return abs(ship.x) + abs(ship.y)
 
 if __name__ == '__main__':
     lines = open('input.txt').readlines()
-
     Step = namedtuple('Step', ['action', 'value'])
-    steps = []
-    for line in lines:
-            matches = re.match("(\w)(\d+)", line)
-            steps.append(Step(matches.group(1), int(matches.group(2))))
-
+    steps = list(Step(m.group(1), int(m.group(2))) for m in map(lambda x: re.match("(\w)(\d+)", x), lines))
     print("PART 1:", part_1(steps))
     print("PART 2:", part_2(steps))
